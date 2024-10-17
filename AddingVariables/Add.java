b6f397f71bcc703e09bc0223380fd659a57753e4
Sobia <sobiakarim2004@gmail.com>
2024-10-17 10:46:11 +0500

    commit

---
+++ AddingVariables/Add.java
@@ -0,0 +1,9 @@
+package AddingVariables;
+
+public class Add {
+    public static void main(String[] args) {
+        int num1 = 2, num2 = 5, num3 = 6 , sum;
+        sum  =  num1+num2+num3;
+        System.out.println("The Sum of " +num1+ " , " +num2+ " and "+num3+ " is " + sum);
+    }
+}
