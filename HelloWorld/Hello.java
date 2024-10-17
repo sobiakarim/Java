8a3f73cfcf1756d9676527fc8b66799e3af74803
Sobia <sobiakarim2004@gmail.com>
2024-10-17 09:36:58 +0500

    commit

--- HelloWorld/Hello.java
+++ HelloWorld/Hello.java
@@ -1,5 +1,25 @@
-public class Hello{
-    public static void main(String[] args) {
-        System.out.print("Hello");
+public class Hello{            //file name and class name must be the same
+    public static void main(String[] args) {     // main = entry point to app
+        System.out.print("Hello World");
     }
 }
+
+
+/*
+function: group of code to do something
+ classes: collection of similar kind of functions 
+ package: similar kind of classes
+
+
+ class -> a template from which we make objects
+ void -> does not return anything
+ main ->  built in function
+ public->access modifier, public can be accessed from anywhere
+ static -> make it possible that we run Class without making its object , independent function, have no concern with any object
+
+
+ naming convention: 
+ classes: PascalConvention
+ functions: camelCaseConvention
+
+ */
